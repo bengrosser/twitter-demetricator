631a93abb153df80f1dacf629add9bb1467b923a
Benjamin Grosser <grosser@safebook.local>
2019-03-26 00:40:45 -0500

    1.1.5 release -- adapting to some Twitter changes that revealed a few metrics

--- chrome/td.user.js
+++ chrome/td.user.js
@@ -113,9 +113,9 @@
     // a few metrics are easy, hidden via CSS. this style is mirrored in 
     // twitterdemetricator.css in order to inject *before* DOM renders on
     // first load, so need to maintain state in these vars plus that file
-    var demetricatedStyle = '.ProfileCardStats-statValue, .ProfileTweet-actionCountForPresentation, .ProfileNav-value, a[data-tweet-stat-count] strong, .ep-MetricAnimation, .ep-MetricValue, .MomentCapsuleLikesFacepile-countNum, .stats li a strong { opacity:0 !important; } .count-wrap { display:hide !important; } div:not(.ProfileTweet-actionList)[aria-label="Tweet actions"] span, div[data-testid="like"] div span, div[data-testid="unlike"] div span, div[data-testid="reply"] div span, div[data-testid="retweet"] div span, div[data-testid="unretweet"] div span, div.rn-z2knda.rn-1wbh5a2 a > div > span:first-child, a.rn-jwli3a span div div, div.rn-7o8qx1 div.rn-axxi2z, a div span span.rn-vw2c0b, span span.rn-jwli3a { display:none; } '; 
+    var demetricatedStyle = '.ProfileCardStats-statValue, .ProfileTweet-actionCountForPresentation, .ProfileNav-value, a[data-tweet-stat-count] strong, .ep-MetricAnimation, .ep-MetricValue, .MomentCapsuleLikesFacepile-countNum, .stats li a strong { opacity:0 !important; } .count-wrap { display:hide !important; } div:not(.ProfileTweet-actionList)[aria-label="Tweet actions"] span, div[data-testid="like"] div span, div[data-testid="unlike"] div span, div[data-testid="reply"] div span, div[data-testid="retweet"] div span, div[data-testid="unretweet"] div span, div.rn-z2knda.rn-1wbh5a2 a > span:first-child, a.rn-jwli3a span div div, div.rn-7o8qx1 div.rn-axxi2z, div a span.rn-vw2c0b, span span.rn-jwli3a { display:none; } '; 
 
-    var inverseDemetricatedStyle = '.ProfileCardStats-statValue, .ProfileTweet-actionCountForPresentation, .ProfileNav-value, a[data-tweet-stat-count] strong, .ep-MetricAnimation, .ep-MetricValue, .MomentCapsuleLikesFacepile-countNum, .stats li a strong { opacity:1 !important; } .count-wrap { display:unset !important; } div:not(.ProfileTweet-actionList)[aria-label="Tweet actions"] span, div[data-testid="like"] div span, div[data-testid="unlike"] div span, div[data-testid="reply"] div span, div[data-testid="retweet"] div span, div[data-testid="unretweet"] div span, div.rn-z2knda.rn-1wbh5a2 a > div > span:first-child, a.rn-jwli3a span div div, div.rn-7o8qx1 div.rn-axxi2z, a div span span.rn-vw2c0b, span span.rn-jwli3a { display:inline !important; } '; 
+    var inverseDemetricatedStyle = '.ProfileCardStats-statValue, .ProfileTweet-actionCountForPresentation, .ProfileNav-value, a[data-tweet-stat-count] strong, .ep-MetricAnimation, .ep-MetricValue, .MomentCapsuleLikesFacepile-countNum, .stats li a strong { opacity:1 !important; } .count-wrap { display:unset !important; } div:not(.ProfileTweet-actionList)[aria-label="Tweet actions"] span, div[data-testid="like"] div span, div[data-testid="unlike"] div span, div[data-testid="reply"] div span, div[data-testid="retweet"] div span, div[data-testid="unretweet"] div span, div.rn-z2knda.rn-1wbh5a2 a > span:first-child, a.rn-jwli3a span div div, div.rn-7o8qx1 div.rn-axxi2z, div a span.rn-vw2c0b, span span.rn-jwli3a { display:inline !important; } '; 
 
     var tweetDeckDemetricatedStyle = 'span.js-ticker-value, .prf-stats li a strong, .like-count, .retweet-count, .reply-count { opacity:0 !important; }';
 
@@ -707,9 +707,15 @@
             // new 2/25/19
             ready('div[data-testid="tweet"]', function(e) {
 
+                //$(e).css('border','2px solid green');
                 if(!newTwitter) return;
+
+                /*
+                 * defunct 3/25 - can't recall why i had this
+                 * but no longer helping/working
                 let singleTweetTest = $(e).css('height');
                 if(singleTweetTest.match(/4/) != null) return;
+                */
 
                 var replyButton = $(e).find('div[data-testid="reply"]');
                 var retweetButton = $(e).find('div[data-testid="retweet"]');
@@ -822,7 +828,9 @@
         // I'm removing some originally embedded markup here
         // but it doesn't seem to matter
         if(newTwitter) {
-            ready('a[aria-label="Followers you know"] div span', function(e) {
+            // defunct 3/25
+            //ready('a[aria-label="Followers you know"] div span', function(e) {
+            ready('a[aria-label="Followers you know"] span', function(e) {
                 if($(e).hasClass("demetricator_checked")) return;
                 else $(e).addClass("demetricator_checked");
 
