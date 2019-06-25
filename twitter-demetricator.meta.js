b7dcea544d30ca6330d445e9b9784ae86c3f95d2
Benjamin Grosser <grosser@safebook.local>
2019-06-25 14:41:13 -0500

    1.1.7 release (fixing new Twitter sidebar)

--- twitter-demetricator.meta.js
+++ twitter-demetricator.meta.js
@@ -1,6 +1,6 @@
 // // ==UserScript==
 // @name        Twitter Demetricator
-// @version     1.1.4
+// @version     1.1.7
 // @namespace   twitterdemetricator
 // @description Hides all the metrics on Twitter
 // @author      Ben Grosser
